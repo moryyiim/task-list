9bbf944d1d50de0b235e8498ac3ab4db1f82641f
Mory Yim <morelenyim@gmail.com>
2022-11-29 20:46:01 -0800

    grab form inputs and verify for input validation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,26 @@
+// Connection successful
+console.log('CONNECTION SUCCESSFUL');
+
 // Selecting form inputs
 
 const name = document.querySelector('#taskName');
-const taskDescription = document.querySelector('#description');
+const description = document.querySelector('#description');
 const assignedTo = document.querySelector('#assignTo');
 const date = document.querySelector('#date');
 
-const taskForm = document.querySelector('#form');
+const form = document.querySelector('#form');
 const submit = document.querySelector('#submit-btn');
+
+// Verifying to see if we are getting data back
+
+const taskName = name.value;
+console.log(`name: ${taskName}`);
+
+const taskDescription = description.value;
+console.log(`description: ${taskDescription}`);
+
+const taskAssignedTo = assignedTo.value;
+console.log(`assigned to: ${taskAssignedTo}`);
+
+const taskDate = date.value;
+console.log(`date: ${taskDate}`);
